e737e8d1d0df53f602dc24258f2fa610245ce55b
s195405 <secaju09@gmail.com>
2024-05-28 20:44:29 -0300

    Arquivos Iniciais

---
+++ model/Pet.js
@@ -0,0 +1,11 @@
+class Pet {
+  constructor(id, petName, breed, speciality, petOwner) {
+    this.id = id
+    this.petName = petName
+    this.breed = breed
+    this.speciality = speciality
+    this.petOwner = petOwner
+  }
+}
+
+export { Pet }
